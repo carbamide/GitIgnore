165722e7127cd1610e2b5c2f80e7f7fb39aed319
Josh Barrow <jbarrow84@me.com>
2015-01-27 17:27:43 -0600

    first commit

---
+++ GitIgnore/File.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  GitIgnore
+//
+//  Created by Joshua Barrow on 1/27/15.
+//  Copyright (c) 2015 Jukaela Enterprises. All rights reserved.
+//
+
+import Foundation
+
+struct Strings {
+    static let TextFieldPlaceholder = "Project type / environment"
+    static let TextFieldTooltip = "Separate entries by single space, such as \"Xcode iOS Objective-C\""
+    static let ButtonTooltip = "Create .gitignore"
+}
