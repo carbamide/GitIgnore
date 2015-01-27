71df60fd1bc5ad76a9c40478688745296bdfe006
Josh Barrow <jbarrow84@me.com>
2015-01-27 12:10:19 -0600

    Initial Commit

---
+++ GitIgnore/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitIgnore
+//
+//  Created by Joshua Barrow on 1/27/15.
+//  Copyright (c) 2015 Jukaela Enterprises. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
